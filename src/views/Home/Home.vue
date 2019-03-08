a801ceb2652ecccb311d87868cb49b2f2522e473
Young <young@yangdeMacBook-Pro.local>
2019-03-08 23:59:31 +0800

    修改了类名为redWall的样式

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -26,5 +26,13 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.redWall {
+  position: absolute;
+  z-index: -1; // 使红墙显示在最底层
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 1.5rem;
+  background-color: #d44439;
+}
 </style>
