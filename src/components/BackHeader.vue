ea8af9595d3328d348eb5d9f0ce6867bb0a3e6be
Young <young@yangdeMacBook-Pro.local>
2019-03-15 20:01:57 +0800

    修改了公共组件-带返回按钮的页面头部组件（BackHeader）

---
+++ src/components/BackHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <Header :class="headerClass">
+    <GoBackBtn/>
+    <span class="back-header-title">{{title}}</span>
+  </Header>
+</template>
+
+<script>
+import GoBackBtn from './GoBackBtn.vue';
+
+export default {
+  data() {
+    return {
+      headerClass: '',
+    };
+  },
+  components: {
+    GoBackBtn,
+  },
+  props: ['title', 'color'],
+  mounted() {
+    this.headerClass = `back-header ${this.color}`;
+  },
+};
+</script>
+
+<style scoped lang='less'>
+.back-header {
+  position: fixed; // FIXME: 固定定位无效
+  z-index: 999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  height: 0.44rem;
+  color: #f1f1f1;
+  &.red {
+    background-color: #d44439;
+  }
+  &.trans {
+    background-color: rgba(0, 0, 0, 0);
+  }
+  .back-header-title {
+    margin: 0 0.05rem;
+    font-size: 0.18rem;
+    font-weight: 400;
+  }
+}
+</style>
