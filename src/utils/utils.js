1d80e05d5572ba56975cf96dc2b0446d76664804
Young <young@yangdeMacBook-Pro.local>
2019-03-22 01:42:56 +0800

    封装了公共方法formatSinger

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,10 @@
-export const getScreenHeight = () => `${document.documentElement.clientHeight / 100  }rem`;
+export const getScreenHeight = () => `${document.documentElement.clientHeight / 100}rem`;
+
+export const formatSinger = (singerList) => {
+  // 拿出列表里面每一项的name，中间用'/'分隔
+  let formatedStr = '';
+  singerList.forEach((item) => {
+    formatedStr += `${item.name}/`;
+  });
+  return formatedStr.slice(0, -1);
+};
