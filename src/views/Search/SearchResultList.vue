a46f8eb3dd530125b29f0286c0955cca51850dc6
Young <young@yangdeMacBook-Pro.local>
2019-03-22 20:02:53 +0800

    暂时放弃修改这个BUG：通过搜索找到的音乐，无法加载其封面图片

--- src/views/Search/SearchResultList.vue
+++ src/views/Search/SearchResultList.vue
@@ -2,7 +2,7 @@
   <ul class="track-list">
     <li class="track-item" v-for="(item, index) in tracks" :key="index">
       <router-link
-        :to="{name: 'song', query: {name: item.name, singers: formatSinger(item.artists), id:item.id}, params: {coverImgUrl: picUrlList[index]}}"
+        :to="{name: 'song', query: {name: item.name, singers: formatSinger(item.artists), id:item.id, album: item.album.id}}"
         class="link-to-play-control"
       >
         <div class="track-info">
@@ -16,32 +16,11 @@
 
 <script>
 import { formatSinger } from '../../utils/utils';
-import { getCoverImgUrl } from '../../api/SearchResultList';
 
 export default {
-  data() {
-    return {
-      picUrlList: [],
-    };
-  },
   props: ['tracks'],
   methods: {
     formatSinger,
-    // 该方法将用于获取封面图片的URL
-    async getData(albumId) {
-      const data = await getCoverImgUrl(albumId);
-      const { picUrl } = data.album;
-      return picUrl;
-    },
-  },
-  mounted() {
-    // 这是一个极其不优雅的小布丁
-    // 从搜索结果列表跳转到播放控制组件的时候，不会在this.$route.params里带上coverImgUrl
-    // 因此需要先发请求请求到coverImgUrl，并写入this.$route.params
-    this.tracks.forEach((item, index) => {
-      this.picUrlList[index] = this.getData(item.album.id);
-      // debugger;
-    });
   },
 };
 </script>
