998aafdbe51f28ea3e9d6fddfb0ec0e3d7d4b19a
Young <young@yangdeMacBook-Pro.local>
2019-03-27 20:27:52 +0800

    修改了axios响应拦截器的一个bug

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = ' http://localhost:3000/';
-axios.interceptors.response.use(response => response.data, (error) => {});
+axios.interceptors.response.use(response => response.data, err => Promise.reject(err.response.data));
 
 export default axios;
