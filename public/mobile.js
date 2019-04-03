bb049690bc145388b4ab36ed511bded967455a54
Young <young@yangdeMacBook-Pro.local>
2019-04-03 23:15:51 +0800

    放弃normalize，改用reset.css；修复移动适配的一点问题

--- public/mobile.js
+++ public/mobile.js
@@ -1,116 +1,44 @@
-(function (win, lib) {
-  const doc = win.document;
-  const docEl = doc.documentElement;
-  let metaEl = doc.querySelector('meta[name="viewport"]');
-  const flexibleEl = doc.querySelector('meta[name="flexible"]');
-  let dpr = 0;
-  let scale = 0;
-  let tid;
-  const flexible = lib.flexible || (lib.flexible = {});
+(function flexible(window, document) {
+  const docEl = document.documentElement;
+  const dpr = window.devicePixelRatio || 1;
 
-  if (metaEl) {
-    console.warn('将根据已有的meta标签来设置缩放比例');
-    const match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
-    if (match) {
-      scale = parseFloat(match[1]);
-      dpr = parseInt(1 / scale);
-    }
-  } else if (flexibleEl) {
-    const content = flexibleEl.getAttribute('content');
-    if (content) {
-      const initialDpr = content.match(/initial\-dpr=([\d\.]+)/);
-      const maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/);
-      if (initialDpr) {
-        dpr = parseFloat(initialDpr[1]);
-        scale = parseFloat((1 / dpr).toFixed(2));
-      }
-      if (maximumDpr) {
-        dpr = parseFloat(maximumDpr[1]);
-        scale = parseFloat((1 / dpr).toFixed(2));
-      }
-    }
-  }
-
-  if (!dpr && !scale) {
-    const isAndroid = win.navigator.appVersion.match(/android/gi);
-    const isIPhone = win.navigator.appVersion.match(/iphone/gi);
-    const { devicePixelRatio } = win;
-    if (isIPhone) {
-      // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-      if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
-        dpr = 3;
-      } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
-        dpr = 2;
-      } else {
-        dpr = 1;
-      }
+  // adjust body font size
+  function setBodyFontSize() {
+    if (document.body) {
+      // document.body.style.fontSize = `${12 * dpr}px`;
+      document.body.style.fontSize = '12px'; // 此处做了修改
     } else {
-      // 其他设备下，仍旧使用1倍的方案
-      dpr = 1;
-    }
-    scale = 1 / dpr;
-  }
-
-  docEl.setAttribute('data-dpr', dpr);
-  if (!metaEl) {
-    metaEl = doc.createElement('meta');
-    metaEl.setAttribute('name', 'viewport');
-    metaEl.setAttribute('content', `initial-scale=${scale}, maximum-scale=${scale}, minimum-scale=${scale}, user-scalable=no`);
-    if (docEl.firstElementChild) {
-      docEl.firstElementChild.appendChild(metaEl);
-    } else {
-      const wrap = doc.createElement('div');
-      wrap.appendChild(metaEl);
-      doc.write(wrap.innerHTML);
+      document.addEventListener('DOMContentLoaded', setBodyFontSize);
     }
   }
+  setBodyFontSize();
 
-  function refreshRem() {
-    let { width } = docEl.getBoundingClientRect();
-    if (width / dpr > 540) {
-      width = 540 * dpr;
-    }
-    const rem = 100;
+  // set 1rem = viewWidth / 3.75
+  function setRemUnit() {
+    const rem = docEl.clientWidth / 3.75; // 此处做了修改
     docEl.style.fontSize = `${rem}px`;
-    flexible.rem = win.rem = rem;
   }
 
-  win.addEventListener('resize', () => {
-    clearTimeout(tid);
-    tid = setTimeout(refreshRem, 300);
-  }, false);
-  win.addEventListener('pageshow', (e) => {
+  setRemUnit();
+
+  // reset rem unit on page resize
+  window.addEventListener('resize', setRemUnit);
+  window.addEventListener('pageshow', (e) => {
     if (e.persisted) {
-      clearTimeout(tid);
-      tid = setTimeout(refreshRem, 300);
+      setRemUnit();
     }
-  }, false);
+  });
 
-  if (doc.readyState === 'complete') {
-    doc.body.style.fontSize = `${12 * dpr}px`;
-  } else {
-    doc.addEventListener('DOMContentLoaded', (e) => {
-      doc.body.style.fontSize = `${12 * dpr}px`;
-    }, false);
+  // detect 0.5px supports
+  if (dpr >= 2) {
+    const fakeBody = document.createElement('body');
+    const testElement = document.createElement('div');
+    testElement.style.border = '.5px solid transparent';
+    fakeBody.appendChild(testElement);
+    docEl.appendChild(fakeBody);
+    if (testElement.offsetHeight === 1) {
+      docEl.classList.add('hairlines');
+    }
+    docEl.removeChild(fakeBody);
   }
-
-
-  refreshRem();
-
-  flexible.dpr = win.dpr = dpr;
-  flexible.refreshRem = refreshRem;
-  flexible.rem2px = function (d) {
-    let val = parseFloat(d) * this.rem;
-    if (typeof d === 'string' && d.match(/rem$/)) {
-      val += 'px';
-    }
-    return val;
-  };
-  flexible.px2rem = function (d) {
-    let val = parseFloat(d) / this.rem;
-    if (typeof d === 'string' && d.match(/px$/)) {
-      val += 'rem';
-    }
-    return val;
-  };
-}(window, window.lib || (window.lib = {})));
+}(window, document));
--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import Vue from 'vue';
-// import MintUI from 'mint-ui';
-// import 'mint-ui/lib/style.css';
-// import { Lazyload } from 'mint-ui';
-// import $ from 'zepto'; /* FIXME: 引入zepto失败 */
-
-import 'normalize.css'; // 引入normalize.css来统一浏览器的默认样式
+// import $ from 'zepto'; /* FIXME: 暂时不能通过这种方式引入zepto */
 import './style/index.less'; // 引入自定义的全局样式
-
 import VueAwesomeSwiper from 'vue-awesome-swiper'; // 引入Vue版本的swiper插件
 import 'swiper/dist/css/swiper.css'; // 引入swiper插件的样式文件
 import App from './App.vue';
@@ -14,9 +8,7 @@ import router from './router';
 import store from './store/index';
 import loading from './assets/loading.jpg';
 
-// Vue.use(MintUI);
-// Vue.use(Lazyload); // 引入lazyload插件
-Vue.use(VueAwesomeSwiper /* { default global options } */);
+Vue.use(VueAwesomeSwiper);
 
 Vue.config.productionTip = false;
 
