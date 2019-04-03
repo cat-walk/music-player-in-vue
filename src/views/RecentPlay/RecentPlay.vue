9cfb5a3dec63269983fa93b404118d593878b3be
Young <young@yangdeMacBook-Pro.local>
2019-04-03 20:21:36 +0800

    修复了“最近播放列表显示错误”的bug

--- src/views/RecentPlay/RecentPlay.vue
+++ src/views/RecentPlay/RecentPlay.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <back-header color="red" title="最近播放"></back-header>
-    <song-list v-if="tracks.length" :tracks="tracks" class="song-list"></song-list>
+    <song-list v-if="recentPlaylist.length" :tracks="tracks" class="song-list"></song-list>
     <p v-else class="empty-remind">你最近没有放过歌噢！</p>
   </section>
 </template>
@@ -13,6 +13,7 @@ import SongList from '../../components/SongList.vue';
 import { getSongDetail } from '../../api/RecentPlay';
 
 export default {
+  name: 'RecentPlay',
   data() {
     return {
       tracks: [],
@@ -21,9 +22,9 @@ export default {
   computed: {
     ...mapGetters(['recentPlaylist']),
   },
-  watch: {
-    async recentPlaylist(newList) {
-      if (newList.length) {
+  methods: {
+    async getRecentPlayList() {
+      if (this.recentPlaylist.length) {
         try {
           const res = await getSongDetail(this.recentPlaylist.toString());
           this.tracks = res.songs;
@@ -32,6 +33,12 @@ export default {
         }
       }
     },
+  },
+  created() {
+    this.getRecentPlayList();
+  },
+  activated() {
+    this.getRecentPlayList();
   },
   components: {
     BackHeader,
