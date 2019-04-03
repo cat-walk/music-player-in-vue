b5b2d3abfaf548843d2c455c1159a2dd108cc011
Young <young@yangdeMacBook-Pro.local>
2019-04-03 20:17:09 +0800

    防止退出登录的接口走缓存

--- src/api/SideBar/index.js
+++ src/api/SideBar/index.js
@@ -1,8 +1,10 @@
-// 由于后台接口不稳定，因此不用发请求的方式来进行获取登录状态和退出登录
 import axios from '../api';
 
 export const logout = () => axios.get('/logout', {
   withCredentials: true,
+  params: {
+    random: Math.random(),
+  },
 });
 
 export const getLoginStatus = () => axios.get('/login/status', {
