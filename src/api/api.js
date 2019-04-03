3d72dc498c804b91a693654fae4d623a05f71104
Young <young@yangdeMacBook-Pro.local>
2019-04-04 00:30:36 +0800

    修复了上线环境的baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // axios.defaults.baseURL = ' http://localhost:3000/';
-axios.defaults.baseURL = ' https://meiyun.info:3000/';
+axios.defaults.baseURL = ' http://meiyun.info:3000/';
 axios.interceptors.response.use(response => response.data, err => Promise.reject(err.response.data));
 
 export default axios;
